6c2e2ce3cf4bf718ce35398acce9b55bc8991cc8
Iñaki <inaki.diaz@ikasle.egibide.org>
2018-01-17 21:58:42 +0100

    Insertar datos en base de datos, conector

---
+++ src/modelo/ConectorMysql.java
@@ -0,0 +1,19 @@
+package modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConectorMysql {
+
+	private static Connection con_mysql_jdbc;
+
+	public static Connection getCon_mysql_jdbc() throws ClassNotFoundException, SQLException {
+	
+		// Cargar el driver
+		Class.forName("com.mysql.jdbc.Driver");
+		// Establecemos la conexion con la BD
+		ConectorMysql.con_mysql_jdbc = DriverManager.getConnection("jdbc:mysql://192.168.33.10/Proyecto2018_db", "root", "root");
+		return con_mysql_jdbc;
+	}
+}
