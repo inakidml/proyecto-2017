4536ccfedb8d1daed8f0d5dae9c379fc64735e91
Unknown <ikerlorentecalvo@gmail.com>
2018-01-24 21:19:55 +0100

    actualizada pagina

--- WebProyecto2/getTemperatura.php
+++ WebProyecto2/getTemperatura.php
@@ -1,15 +1,14 @@
 <?php
 header('Content-Type: application/json');
 
-$fecha = $_REQUEST['fec'];
-echo json_encode("hola" + $fecha,true);
+$query = $_GET['sql'];
 
 $con = mysqli_connect("10.1.100.100", "root", "root", "Proyecto2018_db");
 if (mysqli_connect_errno($con)) {
     echo "Failed to connect to DataBase: " . mysqli_connect_error();
 } else {
     $data_points = array();
-    $result = mysqli_query($con, "SELECT fecha, temperatura FROM registro where fecha like '" + $fecha + "%'"); 
+    $result = mysqli_query($con, $query); 
     while ($row = mysqli_fetch_array($result)) {
         $point = array("fecha" => $row['fecha'], "temperatura" => $row['temperatura']);
         array_push($data_points, $point);
